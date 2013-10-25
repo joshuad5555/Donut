ef2c91b13d8e2173fd4114ca94a746a00798efdc
joshuad5555 <joshuad5555@gmail.com>
2013-10-24 21:19:42 -0700

    Create Reference.java

---
+++ Common/com/joshuad5555/donut/lib/Reference.java
@@ -0,0 +1,8 @@
+package com.joshuad5555.donut.lib;
+
+public class Reference {
+
+    public static final String COMMON_PROXY_PATH = "com.joshuad5555.donut.proxy.CommonProxy";
+    public static final String CLIENT_PROXY_PATH = "com.joshuad5555.donut.proxy.ClientProxy";
+	
+}
